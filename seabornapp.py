ee622336afb3f05b5006cef752fd8d2f81a58722
Carl Graf von Moltke <131779341+CarlGvM@users.noreply.github.com>
2025-03-09 18:49:33 +0100

    Update seabornapp.py

--- seabornapp.py
+++ seabornapp.py
@@ -11,7 +11,7 @@ import random
 import time
 
 
-cope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
 client = gspread.authorize(creds)
 
